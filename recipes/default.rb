bfdaf3c9fb795ac48bb46302efefdb839d6b4dd1
Anthony Caiafa <acaiafa1@bloomberg.net>
2015-10-09 15:14:07 -0400

    ading iptables template

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,15 +17,4 @@ confd_template '/etc/confd/iptables.rules' do
   only_if { node['confd-iptables']['enable_iptables'] }
 end
 
-confd_template '/etc/confd/ip6tables.rules' do
-  source 'iptables.rules.tmpl'
-  keys "/hosts/#{node['ip6address']}"
-
-  check_command "/usr/sbin/ip6tables-restore -T confd -t #{path}"
-  reload_command "/usr/sbin/ip6tables-restore -T confd #{path}"
-
-  notifies :restart, 'confd_service[confd]', :delayed
-  only_if { node['confd-iptables']['enable_ip6tables'] }
-end
-
 include_recipe 'confd::default'
