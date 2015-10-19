6e917e91da52bf46fbd1c941e4d3d789086ad53a
John Bellone <jbellone@bellone.us>
2015-10-19 10:56:34 -0700

    Fixes the Go language template for running in confd.

--- Berksfile
+++ Berksfile
@@ -1,2 +1,8 @@
 source 'https://supermarket.chef.io'
 metadata
+
+group :test, :integration do
+  cookbook 'java', git: 'https://github.com/agileorbit-cookbooks/java'
+  cookbook 'test-confd-iptables', path: 'test/fixture/cookbooks/test-confd-iptables'
+  cookbook 'zookeeper-cluster'
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,18 +4,16 @@
 #
 # Copyright 2015, Bloomberg Finance L.P.
 #
-include_recipe 'iptables::default'
+include_recipe 'iptables::default', 'confd::default'
 
 confd_template '/etc/confd/iptables.rules' do
   template_source node['confd-iptables']['template_source']
   prefix node['confd-iptables']['prefix']
   keys node.tags.map { |t| "/groups/#{t}" }
 
-  check_command "/usr/sbin/iptables-restore -T confd -t #{path}"
-  reload_command "/usr/sbin/iptables-restore -T confd #{path}"
+  check_command "/sbin/iptables-restore -T confd -t #{path}"
+  reload_command "/sbin/iptables-restore -T confd #{path}"
 
   notifies :restart, 'confd_service[confd]', :delayed
   only_if { node['confd-iptables']['enable_iptables'] }
 end
-
-include_recipe 'confd::default'
