ec1a0dd79d3a64f0b6168af4fef3e70ed82392d0
John Bellone <jbellone@bloomberg.net>
2015-10-20 10:00:17 -0400

    Modifies confd to write rules to /etc/iptables.d.

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'jbellone@bloomberg.net'
 license 'Apache 2.0'
 description 'Application cookbook which configures confd iptables template.'
 long_description 'Application cookbook which configures confd iptables template.'
-version '1.0.0'
+version '1.0.1'
 
 supports 'ubuntu', '>= 12.04'
 supports 'redhat', '>= 5.8'
 supports 'centos', '>= 5.8'
 
-depends 'iptables'
 depends 'confd', '~> 1.0'
+depends 'iptables'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,7 +6,7 @@
 #
 include_recipe 'iptables::default', 'confd::default'
 
-confd_template '/etc/confd/iptables.rules' do
+confd_template '/etc/iptables.d/confd' do
   template_source node['confd-iptables']['template_source']
   prefix node['confd-iptables']['prefix']
   keys node.tags.map { |t| "/groups/#{t}" }
@@ -15,5 +15,4 @@ confd_template '/etc/confd/iptables.rules' do
   reload_command "/sbin/iptables-restore -T confd #{path}"
 
   notifies :restart, 'confd_service[confd]', :delayed
-  only_if { node['confd-iptables']['enable_iptables'] }
 end
