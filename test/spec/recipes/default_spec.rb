449a73421a1e05dbaaf8bb6a649ebd0a12870f77
John Bellone <jbellone@bellone.us>
2015-10-20 17:46:20 -0400

    Fixes the tests to make them green.

--- test/spec/recipes/default_spec.rb
+++ test/spec/recipes/default_spec.rb
@@ -5,19 +5,19 @@ describe 'confd-iptables::default' do
     cached(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04') do |node|
         node.tag('default')
-        node.tag('web')
+        node.tag('testing')
       end.converge('confd-iptables::default')
     end
 
-    it { expect(chef_run).to include_recipe('iptables::default') }
     it { expect(chef_run).to include_recipe('confd::default') }
+    it { expect(chef_run).to create_directory('/etc/iptables') }
     it do
-      expect(chef_run).to create_confd_template('/etc/confd/iptables.rules')
+      expect(chef_run).to create_confd_template('/etc/iptables/confd')
         .with(template_source: 'iptables.tmpl.erb')
         .with(prefix: '/')
-        .with(keys: %w{/groups/default /groups/web})
-        .with(check_command: '/usr/sbin/iptables-restore -T confd -t /etc/confd/iptables.rules')
-        .with(reload_command: '/usr/sbin/iptables-restore -T confd /etc/confd/iptables.rules')
+        .with(keys: %w{/groups/default /groups/testing})
+        .with(check_command: '/sbin/iptables-restore -n -t < /etc/iptables/confd')
+        .with(reload_command: '/sbin/iptables-restore -n < /etc/iptables/confd')
     end
   end
 end
