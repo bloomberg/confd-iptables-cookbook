b569fa4e7567cf9845213e7e45bb0dad2ca0f101
John Bellone <jbellone@bellone.us>
2015-11-09 15:11:04 -0500

    Fixes the integration and unit tests.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -16,19 +16,19 @@ describe file('/etc/default/iptables') do
 end
 
 describe iptables do
-  it { should have_rule('-P INPUT DROP') }
+  it { should have_rule('-P INPUT ACCEPT') }
   it { should have_rule('-P FORWARD ACCEPT') }
   it { should have_rule('-P OUTPUT ACCEPT') }
   it { should have_rule('-A INPUT -m state --state ESTABLISHED -j ACCEPT') }
   it { should have_rule('-A INPUT -m state --state RELATED -j ACCEPT') }
   it { should have_rule('-A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT') }
   it { should have_rule('-A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT') }
-  it { should have_rule('-A INPUT -s 127.0.0.1/32 -p tcp -m tcp --dport 22 -j ACCEPT') }
-  it { should have_rule('-A INPUT -s 127.0.1.1/32 -p tcp -m tcp --dport 22 -j ACCEPT') }
-  it { should have_rule('-A INPUT -s 127.0.2.1/32 -p tcp -m tcp --dport 22 -j ACCEPT') }
-  it { should have_rule('-A INPUT -s 127.0.1.1/32 -p tcp -m tcp --dport 80 -j ACCEPT') }
-  it { should have_rule('-A INPUT -s 127.0.1.1/32 -p tcp -m tcp --dport 443 -j ACCEPT') }
-  it { should have_rule('-A INPUT -s 127.0.2.1/32 -p tcp -m tcp --dport 8080 -j ACCEPT') }
-  it { should have_rule('-A INPUT -s 127.0.2.1/32 -p tcp -m tcp --dport 8443 -j ACCEPT') }
+  it { should have_rule('-A INPUT -s 127.0.0.1/32 -p tcp -m tcp --dport 22 -j DEFAULT') }
+  it { should have_rule('-A INPUT -s 127.0.1.1/32 -p tcp -m tcp --dport 22 -j DEFAULT') }
+  it { should have_rule('-A INPUT -s 127.0.2.1/32 -p tcp -m tcp --dport 22 -j DEFAULT') }
+  it { should have_rule('-A INPUT -s 127.0.1.1/32 -p tcp -m tcp --dport 80 -j DEFAULT') }
+  it { should have_rule('-A INPUT -s 127.0.1.1/32 -p tcp -m tcp --dport 443 -j DEFAULT') }
+  it { should have_rule('-A OUTPUT -d 127.0.2.1/32 -p tcp -m tcp --dport 8080 -j DEFAULT') }
+  it { should have_rule('-A OUTPUT -d 127.0.2.1/32 -p tcp -m tcp --dport 8443 -j DEFAULT') }
   it { should have_rule('-A INPUT -j DROP') }
 end
